7f85da7b15364a0526c6537c800561381398ea9e
benjl <>
2024-05-29 02:42:14 -0700

    whoopee rank objects
    get rank:[gamemode]:[rank] to find out who is that rank!

--- apps/backend/src/app/modules/ranks/ranks.service.ts
+++ apps/backend/src/app/modules/ranks/ranks.service.ts
@@ -3,7 +3,7 @@ import { EXTENDED_PRISMA_SERVICE } from '../database/db.constants';
 import { ExtendedPrismaService } from '../database/prisma.extension';
 import { Cron } from '@nestjs/schedule';
 import { createClient } from 'redis';
-import { LeaderboardType } from '@momentum/constants';
+import { Gamemode, LeaderboardType } from '@momentum/constants';
 import { XpSystemsService } from '../xp-systems/xp-systems.service';
 
 Injectable()
@@ -15,6 +15,8 @@ export class RanksService {
 
   @Cron('* * * * *')
   async updateRanks() {
+    const gamemodeArray = [Gamemode.SURF, Gamemode.BHOP, Gamemode.RJ, Gamemode.SJ,
+      Gamemode.AHOP, Gamemode.CONC, Gamemode.DEFRAG_CPM, Gamemode.DEFRAG_VQ3];
     // const leaderboardKey = (l: Leaderboard) => 'leaderboard:' + [l.mapID, l.gamemode, l.trackType, l.trackNum, l.style].join(':');
     // const runKey = (r: LeaderboardRun) => 'run:' + [r.userID, r.gamemode, r.style, r.mapID, r.trackType, r.trackNum].join(':');
 
@@ -42,61 +44,59 @@ export class RanksService {
 
     const t1 = Date.now();
     console.log(`Runs loaded. Ranking players...(${t1-t0}ms)`);
-
-    const users: {[id: string]: [number, number, number, number, number, number, number, number, number, number, number, number]} = {};
+    const userPoints: {[id: string]: {[K in Gamemode]: number}} = {};
     for (const lb of lbs) {
       const sortedRuns = lb.runs.sort((a, b) => a.time - b.time);
       let rank = 1;
       const completions = sortedRuns.length;
       for (const run of sortedRuns) {
         const pts = this.xp.getRankXpForRank(rank, completions);
-        if (!(`user:${run.userID}` in users)) {
-          users[`user:${run.userID}`] = [0,0,0,0,0,0,0,0,0,0,0,0];
+        if (!(`user:${run.userID}` in userPoints)) {
+          userPoints[`user:${run.userID}`] = {1: 0, 2: 0, 4: 0, 5: 0, 7: 0, 9: 0, 10: 0, 11: 0};
         }
-        users[`user:${run.userID}`][run.gamemode] += pts.rankXP;
+        userPoints[`user:${run.userID}`][run.gamemode] += pts.rankXP;
         rank++;
       }
     }
-    const userRanks: {[id: string]: [number, number, number, number, number, number, number, number, number, number, number, number]} = {};
-    const userIDs: string[] = [];
-    for (const uid in users) {
-      userIDs.push(uid);
+    const rankedUserIDs: string[] = [];
+    for (const userID in userPoints) {
+      rankedUserIDs.push(userID);
     }
-    const sortedUsers: string[][] = [[], [], [], [], [], [], [], [], [], [], [], []];
-    for (const gamemode of [1, 2, 4, 5, 7, 9, 10, 11]) {
-      sortedUsers[gamemode].push(...userIDs.filter((x) => users[x][gamemode] > 0)
-        .sort((a, b) => users[b][gamemode] - users[a][gamemode]));
+    const userRanks: {[id: string]: {[K in Gamemode]: number}} = {};
+    for (const gamemode of gamemodeArray) {
+      const rankedUsers: string[] = rankedUserIDs.filter((x) => userPoints[x][gamemode] > 0)
+        .sort((a, b) => userPoints[b][gamemode] - userPoints[a][gamemode]);
       let rank = 1;
-      for (const u in sortedUsers) {
-        if (!(u in userRanks)) {
-          userRanks[u] = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1];
+      for (const user of rankedUsers) {
+        if (!(user in userRanks)) {
+          userRanks[user] = {1: -1, 2: -1, 4: -1, 5: -1, 7: -1, 9: -1, 10: -1, 11: -1};
         }
-        userRanks[u][gamemode] = rank;
+        userRanks[user][gamemode] = rank;
         rank++;
       }
     }
+
     const t2_sortingDone = Date.now();
-    console.log(`Sorting done. Pushing to redis... (${t2_sortingDone-t0}ms)`);
+    console.log(`Sorting done. Pushing to redis... (${t2_sortingDone-t1}ms)`);
     await rb.executeIsolated(async (rbtx) => {
-      for (const gamemode of [1, 2, 4, 5, 7, 9, 10, 11]) {
-        let rank = 1;
-        for (const usr of sortedUsers[gamemode]) {
-          await rbtx.set(`rank:${gamemode}:${rank}`, usr);
-          rank++;
+      await rbtx.flushDb();
+      const promises: any[] = [];
+      for (const user in userPoints) {
+        promises.push(rbtx.hSet(user,
+          {'gamemode:1': userPoints[user][1], 'gamemode:2': userPoints[user][2],
+          'gamemode:4': userPoints[user][4], 'gamemode:5': userPoints[user][5],
+          'gamemode:7': userPoints[user][7], 'gamemode:9': userPoints[user][9],
+          'gamemode:10': userPoints[user][10], 'gamemode:11': userPoints[user][11]
+        }));
+      }
+      for (const user in userRanks) {
+        for (const gamemode of gamemodeArray) {
+          if (userRanks[user][gamemode] !== -1) {
+            promises.push(rbtx.set(`rank:${gamemode}:${userRanks[user][gamemode]}`, user));
+          }
         }
       }
-      for (const user of userIDs) {
-        await rbtx.hSet(user,
-          {'gamemode:1': users[user][1], 'gamemode:2': users[user][2],
-          'gamemode:4': users[user][4], 'gamemode:5': users[user][5],
-          'gamemode:7': users[user][7], 'gamemode:9': users[user][9],
-          'gamemode:10': users[user][10], 'gamemode:11': users[user][11]
-          // 'rank:1': userRanks[user][1], 'rank:2': userRanks[user][2],
-          // 'rank:4': userRanks[user][4], 'rank:5': userRanks[user][5],
-          // 'rank:7': userRanks[user][7], 'rank:9': userRanks[user][9],
-          // 'rank:10': userRanks[user][10], 'rank:11': userRanks[user][11]
-        });
-      }
+      await Promise.all(promises);
     });
     await rb.quit();
     const t2 = Date.now();
